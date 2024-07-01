7b803b0398b367e79895a279373af39cc6dd784b
Almndrz02 <almendarizdaniel456@gmail.com>
2024-07-01 16:50:13 -0600

    Chicharronera

---
+++ chicharronerra.py
@@ -0,0 +1,8 @@
+a = float(input('Ingresa el valor A  '))
+b = float(input('Ingresa el valor B  '))
+c = float(input('Ingresa el valor C  '))
+
+x1 = (-b + (b*b - 4*a*c)**0.5)/(2*a) 
+x2 = (-b - (b*b - 4*a*c)**0.5)/(2*a)
+
+print (x1, ' ', x2) 
